1dd5a95f131ea7d63ce95252936574ef57bc1b1c
Haile <118998054+blackmammoth@users.noreply.github.com>
2024-11-26 19:18:03 +0100

    added readme and vercel config

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Steps to Run Locally
+
+1. Clone the github repository.
+2. Run `npm install`
+3. Run `npm run dev` to start the environment in dev mode.
+4. Run `npm run build` to build the project and get the compiled files (found in the folder `\dist`)
+5. Run `npm run start` to start the project using only the compiled js files.
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import bookRoutes from './routes/bookRoutes';
+import bookRoutes from "./routes/bookRoutes";
 
 dotenv.config();
-
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use("/api", bookRoutes); // prefixes all routes with /api
+app.use("/api", bookRoutes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
