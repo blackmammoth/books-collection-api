92e96ce73781de1b6f6bd0366c8868db278e0ee2
Haile <118998054+blackmammoth@users.noreply.github.com>
2024-11-26 13:31:20 +0100

    setup-basic-endpoints

---
+++ src/db.ts
@@ -0,0 +1,24 @@
+
+import { MongoClient, Db } from 'mongodb';
+import dotenv from "dotenv";
+
+dotenv.config();
+const uri = process.env.DB_CONN_STRING || 'connection-string';
+
+let client: MongoClient;
+let db: Db;
+
+console.log("Process env: ", process.env.DB_NAME)
+
+export const connectDB = async () => {
+  if (!client) {
+    client = new MongoClient(uri);
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+  }
+  return db;
+};
+
+export const closeDB = async () => {
+  if (client) await client.close();
+};
